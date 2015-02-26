72cfeba4101b044a1d894e9a853e5899c4a43aee
Enrique Ruiz <enrique.ruiz@mymts.net>
2015-02-26 15:39:44 -0600

    Fetch latest updates from upstream.

--- node_modules/grunt-check-dependencies/tasks/check-dependencies.js
+++ node_modules/grunt-check-dependencies/tasks/check-dependencies.js
@@ -8,14 +8,39 @@
 
 'use strict';
 
-module.exports = function (grunt) {
-    var checkDependencies = require('./lib/check-dependencies')(grunt);
+var cloneDeep = require('lodash.clonedeep'),
+    checkDependencies = require('check-dependencies');
 
+module.exports = function (grunt) {
     grunt.registerMultiTask('checkDependencies',
-        'Checks if currently installed npm dependencies are installed in the exact same versions ' +
+            'Checks if currently installed npm dependencies are installed in the exact same versions ' +
             'that are specified in package.json',
         function () {
-            return checkDependencies(this.options(), this.async());
+            var options = cloneDeep(this.options()),
+                done = this.async();
+
+            if (options.verbose) {
+                options.log = grunt.log.writeln;
+            } else {
+                options.log = function () {};
+            }
+            options.error = grunt.log.error;
+
+            // Our verbose mode represents check-dependencies verbose mode but even in non-verbose
+            // mode we want to have error messages logged so from check-dependencies perspective
+            // we're always verbose.
+            options.verbose = true;
+
+            checkDependencies(options, function (output) {
+                // The checkDependencies function succeeds if dependencies were mismatched
+                // but `npm install` ended fine; however, in case of mismatch we might have
+                // obsolete Grunt tasks loaded so it's better to fail this time and require
+                // a re-run.
+                if (output.status === 0 && !output.depsWereOk) {
+                    grunt.log.error('Dependencies have been updated. Please re-run your Grunt task.');
+                }
+                done(output.status === 0 && output.depsWereOk);
+            });
         }
     );
 };
